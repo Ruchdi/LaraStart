27e8354e54ffba722a4142f453d5b4fccd6a741b
RuchdiR <ruchdir@bfsasia.com>
2019-08-29 09:48:54 +0700

    Create Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,30 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+
+//When used with a module system, you must explicitly install the router via Vue.use():
+
+//import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+let routes = [
+    { path: '/dashboard', component: require('./components/DashboardComponent.vue').default },
+    { path: '/profile', component: require('./components/ProfileComponent.vue').default }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +53,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
