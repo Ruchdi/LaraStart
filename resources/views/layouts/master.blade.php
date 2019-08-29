27c140ab75c8340bd092ac127e7c8f87ad32e833
RuchdiR <ruchdir@bfsasia.com>
2019-08-29 10:34:47 +0700

    Custom Color for Font Awesome Icons and How Logout Works in Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,7 +51,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-            <img src="./img/logo.png" alt="LaraStart Logo" class="brand-image img-circle elevation-3"
+            <img src="{{ asset('img/logo.png') }}" alt="LaraStart Logo" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
             <span class="brand-text font-weight-light">LaraStart</span>
             </a>
@@ -61,7 +61,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('img/profile.png') }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block">
@@ -77,7 +77,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     with font-awesome or any other icon font library -->
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <i class="nav-icon fas fa-tachometer-alt text-blue"></i>
                             <p>Dashboard</p>
                         </router-link>
                     </li>
@@ -110,10 +110,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-power-off"></i>
-                        <p>Log Out</p>
+                        <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-power-off text-red"></i>
+                            <p>{{ __('Logout') }}</p>
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
