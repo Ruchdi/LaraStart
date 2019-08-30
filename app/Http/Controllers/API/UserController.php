5c446b81c30ac5aa03ff11b38de27413a9d0b41c
RuchdiR <ruchdir@bfsasia.com>
2019-08-30 15:22:00 +0700

    All we need is JWT for Laravel API Authentication

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        //You must learn about laravel passport
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
