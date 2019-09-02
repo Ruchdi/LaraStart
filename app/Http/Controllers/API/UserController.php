49f55af40bf5ad2666357c40d063010a846afffd
RuchdiR <ruchdir@bfsasia.com>
2019-09-02 14:33:00 +0700

    Delete Old Photo From Server if User Change Profile Photo

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -70,24 +70,35 @@ class UserController extends Controller
     public function updateProfile(Request $request)
     {
         $user = auth('api')->user();
-        $currentPhoto = $user->photo;
         $this->validate($request,[
             'name'  => 'required|string|max:191',
             'email' => 'required|string|email|max:191|unique:users,email,'.$user->id,
             'password'  =>  'sometimes|min:6',
         ]);
+
+        $currentPhoto = $user->photo;
         if($request->photo != $currentPhoto){
             //generate photo name
             //(explode('/', (explode(';', $request->photo))[0]))[1] for get an extension
             $name = time().'.'.(explode('/', (explode(';', $request->photo))[0]))[1];
+
             //upload photo
             \Image::make($request->photo)->save(public_path('img/profile/').$name);
+
             //assign new photo name
             $request->merge(['photo' => $name]);
+
+            //find path of the old photo
+            $old_photo = public_path('img/profile').$currentPhoto;
+            if(file_exists($old_photo)){
+                @unlink($old_photo);
+            }
         }
+
         if(!empty($request->password)){
             $request->merge(['password'=>Hash::make($request['password'])]);
          }
+
         $user->update($request->all());
         return ['message' => 'success'];
     }
